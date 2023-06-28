fa546020cd3809504cd760b39dbfadfac8110efd
alexis77360 <84974466+alexis77360@users.noreply.github.com>
2023-06-28 15:32:07 +0200

    bubble creator

---
+++ index.js
@@ -0,0 +1,50 @@
+
+const bubbleCreator = () => {
+//? Création de la bulle
+const bubble = document.createElement("span");
+
+bubble.classList.add("bubble");
+document.body.appendChild(bubble);
+
+//? Permet de faire des tailles aléatoires entre 100 et 300
+const size = Math.random() * 200 + 100 + "px";
+
+bubble.style.height = size;
+bubble.style.width = size;
+
+//? position aléatoire sur la hauteur 
+bubble.style.top = Math.random() * 100 + 50+  "%";
+
+//? position aléatoire sur la largeur
+bubble.style.left = Math.random() * 100 + "%";
+
+//? permet de faire positif ou négatif
+const plusMinus = Math.random() > 0.5 ? 1 : -1;
+
+//? Déporte la bulle vers la gauche avec la variable dans le css 
+bubble.style.setProperty("--left", Math.random() * 100 * plusMinus + "%");
+
+
+//? Supprime la bulle au click
+bubble.addEventListener("click", () => {
+    bubble.remove();
+});
+
+
+//? Supprime automatiquement la bulle au bout de x secondes
+setTimeout(() => {
+    bubble.remove();
+}, 8000);
+
+
+
+
+
+};
+
+//? création de bulle toutes les x secondes
+
+setInterval(() => {
+    bubbleCreator();
+}
+, 3020);
